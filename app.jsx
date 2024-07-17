1ee324d4d36ff86ccb5083d8af8194e03759df3c
Adhyan Aditya <115368410+OmeGa8989@users.noreply.github.com>
2024-07-17 11:16:46 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,9 +1,13 @@
+import Adhyan from "./adhyan"
+
 function App() {
   return (
-    <h1>
-      Welcome to React. This is a basic template of the code furthur to be edited.
-    </h1>
-  );
+    <>
+      <h1>Welcome to ViteReact | Adhyan8p</h1>
+      <Adhyan />
+    </>
+
+  )
 }
 
-export default App;
+export default App
