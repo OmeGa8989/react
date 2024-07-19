89ddc5c8068077100a0ad39cf8abac84594b4c05
Adhyan Aditya <115368410+OmeGa8989@users.noreply.github.com>
2024-07-19 09:32:06 +0530

    Counter Variable project

---
+++ counterProject.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0); //default value in useState(default value of counter)
+  let addValue = () => {
+    setCounter(counter + 1)
+    console.log(counter);
+  }
+  let subValue = () => {
+    setCounter(counter - 1)
+    console.log(counter);
+  }
+  return (
+    <div>
+
+      <h1>A counter Project!</h1>
+      <h3>Counter value:{counter}</h3>
+      <br />
+      <button onMouseEnter={addValue}>++</button>
+      <button onMouseEnter={subValue}>--</button>
+      <footer>Counter is :{counter}</footer>
+
+    </div>
+  )
+
+}
+
+export default App
