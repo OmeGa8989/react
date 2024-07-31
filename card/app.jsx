73c233e68df23d7e79e386cd21cd0ccea3eec739
Adhyan Aditya <115368410+OmeGa8989@users.noreply.github.com>
2024-08-01 03:11:31 +0530

    Update app.jsx

--- card/app.jsx
+++ card/app.jsx
@@ -9,19 +9,21 @@ function App() {
 
   let myProp = {
     name : "Adhyan",
-    tech : 1
+    tech : 1,
   }
+  
 
  let arr=[1,2,3,4,5]
 
   return (
     <>
-      <h1 className=' font-mono text-yellow-300 bg-blue-400 rounded-lg text-5xl mb-4 p-2 '>My Card</h1>
-      <Card name = 'myProp1' type = {1} objReturn={myProp}/>
-      <Card name = 'myProp2' type = {2} objReturn={arr}/>
+      <h1 className='  text-left font-mono text-yellow-300 bg-pink-400 rounded-lg text-5xl mb-4 p-2 '>My Card ⬇️</h1>
+      <Card name = 'Adhyan' h1= "This is 1st page"  type = {1} objReturn={myProp}/>
+      <Card name = 'Aditya' h1= "This is 2nd page"  type = {2}  objReturn={arr}/>
     
     </>
   )
+  
 }
 
 export default App
