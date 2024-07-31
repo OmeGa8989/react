31144519539874af75636fb9cfa260600923c2bf
Adhyan Aditya <115368410+OmeGa8989@users.noreply.github.com>
2024-08-01 00:20:05 +0530

    Create Card.jsx
    Card Made using react and twcss

---
+++ card/Card.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let myProp = {
+    name : "Adhyan",
+    tech : 1
+  }
+
+ let arr=[1,2,3,4,5]
+
+  return (
+    <>
+      <h1 className=' font-mono text-yellow-300 bg-blue-400 rounded-lg text-5xl mb-4 p-2 '>My Card</h1>
+      <Card name = 'myProp1' type = {1} objReturn={myProp}/>
+      <Card name = 'myProp2' type = {2} objReturn={arr}/>
+    
+    </>
+  )
+}
+
+export default App
