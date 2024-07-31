7a80727b3fe87791dc6d2dfc17be8c53f0313c5a
Adhyan Aditya <115368410+OmeGa8989@users.noreply.github.com>
2024-08-01 03:11:16 +0530

    Update Card.jsx

--- card/Card.jsx
+++ card/Card.jsx
@@ -2,12 +2,14 @@
 
 import React from "react";
 
-function Card({name , type}){
-    console.log('name: ', name );
+function Card({name, type,h1}){
+    // console.log('name: ', name );
     return(
-        <figure className="md:flex mb-5 rounded-xl p-8 md: dark: bg-gradient-to-r from-slate-700 to-slate-900">
+        <>
+        <figure className="  md:flex mb-5 rounded-xl p-8 md: dark: bg-gradient-to-r from-slate-700 to-slate-900">
         <img className="w-24 h-24 scale-[1.25] mt-5 rounded mx-auto" src="https://images.pexels.com/photos/2837579/pexels-photo-2837579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512"></img>
         <div className="pt-6 text-center space-y-4">
+        <button className="   ml-3  font-bold text-sky-300 "> {h1} </button>
           <blockquote>
             <p className="text-lg font-medium ml-2">
           "A sunset paints the sky with hues of orange and pink, casting a warm glow over the horizon. Silhouettes of trees and mountains stand stark against the vibrant backdrop, creating a serene and picturesque scene. As the sun dips below the horizon, the colors fade into twilight, ushering in the calm of evening."
@@ -16,14 +18,14 @@ function Card({name , type}){
           <figcaption className="font-medium">
             <div className="text-sky-500 dark:text-sky-400">
             Adhyan Aditya
-              
             </div>
             <div className="text-slate-700 dark:text-slate-500">
-            Aspiring SDE {name} , {type}
+            Aspiring {name}, SDE, {type}
             </div>
           </figcaption>
         </div>
       </figure>
+      </>
     )
 }
 
